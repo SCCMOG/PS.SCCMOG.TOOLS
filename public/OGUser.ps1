0f34f7e7c8fb66f78307af9de2bcce11021c45ff
SCCMOG <rkcsjsy@gmail.com>
2021-11-25 11:49:53 +0000

    Get-OGLoggedOnUserWMI

--- public/OGUser.ps1
+++ public/OGUser.ps1
@@ -158,6 +158,114 @@ function Get-OGLoggedOnUser{
     return $CurrentLoggedOnUser
 }
 
+
+<#
+.SYNOPSIS
+    Gets logged on user from Win32_UserProfile
+.DESCRIPTION
+    This function gets all users that are currently connected to a device using Win32_UserProfile and then returns the Active user as a custom PS Object.
+.EXAMPLE
+    PS C:\> Get-OGLoggedOnUserWMI
+    
+    Returns a $CurrentLoggedOnUser Object with properties:
+
+            USERNAME          : rkcsj
+            USERPROFILE       : C:\Users\rkcsj
+            USERDOMAIN        : RSNUC
+            SID               : S-1-5-21-1291444473-292444476-2264444970-1001
+            DNSDOMAIN         : SCCMOG.local
+            LOGONSERVER       : \\RSNUC
+            HOMEPATH          : \Users\rkcsj
+            HOMEDRIVE         : C:
+            APPDATA           : C:\Users\rkcsj\AppData\Roaming
+            LOCALAPPDATA      : C:\Users\rkcsj\AppData\Local
+            Domain_SamAccount : RSNUC\rkcsj
+            AADUserName       : richie.schuster@sccmog.com
+            AADUserObjID      : 68fg9e49-4b39-4f13-a0bf-79b4f94d0691
+
+.OUTPUTS
+    Returns a $CurrentLoggedOnUser PowerShell Object with properties:
+        
+            USERNAME          : rkcsj
+            USERPROFILE       : C:\Users\rkcsj
+            USERDOMAIN        : RSNUC
+            SID               : S-1-5-21-1291444473-292444476-2264444970-1001
+            DNSDOMAIN         : SCCMOG.local
+            LOGONSERVER       : \\RSNUC
+            HOMEPATH          : \Users\rkcsj
+            HOMEDRIVE         : C:
+            APPDATA           : C:\Users\rkcsj\AppData\Roaming
+            LOCALAPPDATA      : C:\Users\rkcsj\AppData\Local
+            Domain_SamAccount : RSNUC\rkcsj
+            AADUserName       : richie.schuster@sccmog.com
+            AADUserObjID      : 68fg9e49-4b39-4f13-a0bf-79b4f94d0691
+
+.NOTES
+    Name:        Get-OGLoggedOnUserWMI   
+    Author:      Richie Schuster - SCCMOG.com
+    Website:     https://www.sccmog.com
+    Contact:     @RichieJSY
+    Created:     2021-11-25
+    Updated:     -
+    
+    Version history:
+    1.0.0 - 2021-11-25 Function created
+#>  
+function Get-OGLoggedOnUserWMI{
+    [cmdletbinding()]
+    $CurrentLoggedOnUser = $null
+    Write-OGLogEntry -Logtext "Getting currently logged from WMI on user for machine: $($ENV:COMPUTERNAME)"
+    $ActiveUser = Get-WmiObject -Class Win32_UserProfile | Where-Object {($_.SID -notmatch “^S-1-5-\d[18|19|20]$”)} | Sort-Object -Property LastUseTime -Descending | Select-Object -First 1
+    if ($ActiveUser.Loaded){
+            $SID_RegVirtualEnv = Get-ItemProperty "Registry::hku\$($ActiveUser.SID)\Volatile Environment" -ErrorAction SilentlyContinue
+            if($SID_RegVirtualEnv)
+            {
+                $MSO365UserIdentityRoot = "Registry::hku\$($ActiveUser.SID)\SOFTWARE\Microsoft\Office\16.0\Common\Identity\Identities"
+                $MSIdentityCacheCurrentUser = "Registry::hklm\SOFTWARE\Microsoft\IdentityStore\Cache\$($ActiveUser.SID)\IdentityCache\$($ActiveUser.SID)"
+                if(Test-Path $MSO365UserIdentityRoot){
+                    $MSO365UserIdentityRoot = Get-ChildItem "$MSO365UserIdentityRoot" | Where-Object {$_.Name -like "*_ADAL"}
+                    if ($MSO365UserIdentityRoot){
+                        $objLoggedOnUserADAL = $MSO365UserIdentityRoot | Select-Object -First 1
+                        $AADUName = "$($objLoggedOnUserADAL.GetValue("EmailAddress"))"
+                        $AADObjID = "$($objLoggedOnUserADAL.GetValue("ProviderId"))"
+                    }
+                }
+                if ($AADUName -like ""){
+                    if(Test-Path $MSIdentityCacheCurrentUser) {
+                        $objCurrentUserMSCachedIdentity = Get-ItemProperty "$MSIdentityCacheCurrentUser"
+                        $AADUName = "$($objCurrentUserMSCachedIdentity.UserName)"
+                        $AADObjID = "NA"
+                    }
+                    else{
+                        $AADUName = "NA"
+                        $AADObjID = "NA"
+                    }
+                }
+                $LoggedInUser  = New-Object psobject
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name USERNAME -Value            $SID_RegVirtualEnv.USERNAME
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name USERPROFILE -Value         $SID_RegVirtualEnv.USERPROFILE
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name USERDOMAIN -Value          $SID_RegVirtualEnv.USERDOMAIN
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name SID -Value                 $ActiveUser.SID
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name DNSDOMAIN -Value           $SID_RegVirtualEnv.USERDNSDOMAIN
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name LOGONSERVER -Value         $SID_RegVirtualEnv.LOGONSERVER
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name HOMEPATH -Value            $SID_RegVirtualEnv.HOMEPATH
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name HOMEDRIVE -Value           $SID_RegVirtualEnv.HOMEDRIVE
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name APPDATA -Value             $SID_RegVirtualEnv.APPDATA
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name LOCALAPPDATA -Value        $SID_RegVirtualEnv.LOCALAPPDATA
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name Domain_SamAccount -Value   "$($SID_RegVirtualEnv.USERDOMAIN)\$($SID_RegVirtualEnv.USERNAME)"
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name Email -Value               $AADUName
+                $LoggedInUser | Add-Member -MemberType NoteProperty -Name AADUserObjID -Value        $AADObjID
+                $CurrentLoggedOnUser = $LoggedInUser
+            }
+    }
+    Else{
+        Write-OGLogEntry -Logtext "ERROR No logged on user for machine: $($ENV:COMPUTERNAME)" -logType Error
+        return $CurrentLoggedOnUser
+    }
+    Write-OGLogEntry -Logtext "Current logged on user: $($CurrentLoggedOnUser.USERNAME)"
+    return $CurrentLoggedOnUser
+}
+
 <#
 .SYNOPSIS
 Launches the specified file as the current logged on user if running from the 'NT AUTHORITY\SYSTEM' context.
@@ -496,7 +604,8 @@ namespace Runasuser
 #Get-ChildItem function: | Where-Object { ($currentFunctions -notcontains $_)-and($_.Name -like "*-OG*") } | Select-Object -ExpandProperty name
 $Export = @(
     "Get-OGLoggedOnUser",
-    "Invoke-OGStartProcessAsCurrentUser"
+    "Invoke-OGStartProcessAsCurrentUser",
+    "Get-OGLoggedOnUserWMI"
 )
 
 foreach ($module in $Export){
